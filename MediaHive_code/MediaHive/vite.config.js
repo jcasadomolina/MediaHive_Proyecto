ddfda1ab44ccf5aec621b47be34d28bd728306a9
José Antonio Casado Molina <jcasadomolina@uma.es>
2024-06-03 18:49:15 +0200

    a

--- MediaHive_code/MediaHive/vite.config.js
+++ MediaHive_code/MediaHive/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html' // Especifica aquí tu archivo HTML como punto de entrada
+    }
+  }
 })
